74a89fcc1cc6b99f5b49506803d04cee77d6b2e0
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-08-27 14:51:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if "logged_in" not in st.session_state:
 def load_designs_from_sheet():
     try:
         sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4mWSP3s6r7UIwn-kcX8Ogev4yXWTMpMLvL87PGTR_UwxKjkcbU9NNxy__mbkyYplhDHxvsD2nKFvW/pub?gid=0&single=true&output=csv
-        response = requests.get(sheet_url, timeout=5)
+    "    response = requests.get(sheet_url, timeout=5)
         if response.status_code == 200:
             df = pd.read_csv(io.StringIO(response.text))
             column_name = 'ITEM NAME' if 'ITEM NAME' in df.columns else df.columns[0]
