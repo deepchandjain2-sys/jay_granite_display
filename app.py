89fb8cf3891b4da61deba77b1658b2ebe3676154
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-10 00:45:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ elif menu == "Display & Item Entry":
     # Fetch live item master from Google Sheet CSV export link
     def fetch_item_master_from_sheet():
         try:
-            sheet_url = "https://docs.google.com/spreadsheets/d/1qhlBmCIUDAKfMX/export?format=csv&gid=0"
+            sheet_url = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZHphemlyd3hheGFibnBwY2hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2ODAyOTgsImV4cCI6MjEwNDI1NjI5OH0.CSCbuwInWJtGpL7w_nMFU6ElGWnXxr67bKeMWuTpMMM"
             df_sheet = pd.read_csv(sheet_url)
             items = df_sheet.iloc[:, 0].dropna().astype(str).tolist()
             return items
