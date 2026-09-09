391e223efaaacfdcf71ba0d24ac71e4750d4436d
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-09 19:06:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import os
 import json
 import base64
 import requests
+SUPABASE_URL = st.secrets.get("SUPABASE_URL")
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.set_page_config(page_title="Jay Granite Tiles Display", layout="wide")
 
@@ -25,9 +28,6 @@ def fetch_users():
 
 # Supabase Connection Setup
 
-SUPABASE_URL = st.secrets.get("SUPABASE_URL")
-SUPABASE_KEY = st.secrets.get("SUPABASE_KEY")
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)    
 default_users = {
     "admin": {"password": "123", "mobile": "9999999999", "role": "admin"},
     "DEEPCHAND JAIN": {"password": "deep1965", "mobile": "9888888888", "role": "admin"}
