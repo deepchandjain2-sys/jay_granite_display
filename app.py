1677f2daf48dcd34a186b893c856a9f3f5303c06
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-10 00:22:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from supabase import create_client, Client
 
+
 # Page Configuration for Professional Look
 st.set_page_config(page_title="Jay Granite Tiles - Management System", layout="wide")
 
