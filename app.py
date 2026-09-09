d626f466dd385de26a858a0f966d4726ce06cd1f
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-09 18:31:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ SUPABASE_KEY = st.secrets.get("SUPABASE_KEY")
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)    
       except:
-          pass
-    
-     return default_val
+        pass
+      return default_val
+     
 def save_json_to_github(filename, data):
     with open(filename, "w") as f:
         json.dump(data, f, indent=4)
