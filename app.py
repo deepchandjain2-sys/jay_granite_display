cb95e8120e4f654ca67e907cb144687e9f68f30a
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-10 00:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,15 +118,15 @@ elif menu == "Display & Item Entry":
        import json
 
 # Function to fetch live item names from Google Sheet
-def fetch_item_master_from_sheet():
-    try:
+      def fetch_item_master_from_sheet():
+          try:
         # Aapki sheet ka proper CSV export link
-        sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4mWSP3s6r7UIwn-kcX8Ogev4yXWTMpMLvL87PGTR_UwxKjkcbU9NNxy__mbkyYplhDHxvsD2nKFvW/pub?gid=1816720040&single=true&output=csv"
-        df_sheet = pd.read_csv(sheet_url)
-        items = df_sheet.iloc[:, 0].dropna().astype(str).tolist()
-        return items
-    except Exception as e:
-        return [
+             sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4mWSP3s6r7UIwn-kcX8Ogev4yXWTMpMLvL87PGTR_UwxKjkcbU9NNxy__mbkyYplhDHxvsD2nKFvW/pub?gid=1816720040&single=true&output=csv"
+             df_sheet = pd.read_csv(sheet_url)
+             items = df_sheet.iloc[:, 0].dropna().astype(str).tolist()
+             return items
+       except Exception as e:
+           return [
             "1000 L 12X18 KK",
             "10015 16X16 CIBELA",
             "1002 CIGAR GLOSSY 1X1 ICON"
